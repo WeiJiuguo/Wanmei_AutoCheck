bfbee51df40d9594d4c75f538cd8fbdef9d62fcd
Yooking <wangyukun55555@gmail.com>
2020-07-08 16:28:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 base_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
 
-with open('11.json', 'r') as f:
+with open('info.json', 'r') as f:
     json = json.load(f)
        
 response = requests.post(base_url, json=json)
