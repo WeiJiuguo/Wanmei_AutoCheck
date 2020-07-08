33a143f43d5895c0fb53a30c9d2172884d5d1385
Yooking <wangyukun55555@gmail.com>
2020-07-08 16:50:23 +0800

    secrets

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import time
 import json
 import requests
 
-base_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
+deptId = eval(input())
+text = input()
+emergencyNum = input()
+
+area = {'address': address, 'text': addtext, 'code': "430321"}
+areaStr = json.dumps(area, ensure_ascii=False)
+
+sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
 
 with open('info.json', 'r') as f:
     json = json.load(f)
        
-response = requests.post(base_url, json=json)
+response = requests.post(sign_url, json=json)
 print(response.text)
 
