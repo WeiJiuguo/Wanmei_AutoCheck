f6c3ace4e97a7a6ccf9b3dedae5169795d73be9b
Yooking <wangyukun55555@gmail.com>
2020-07-08 15:25:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,7 @@ import requests
 
 base_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
 
-text = input()
-address = input()
-addtext = input()
-deptId = eval(input())
-userId = 9022415
-stuNum = 201812010301
-userName = 王宇坤
-phoneNum = 15538550843
-
-area = {'address': address, 'text': addtext, 'code': "430321"}
-
-areaStr = json.dumps(area, ensure_ascii=False)
-
-json = {"businessType": "epmpics", "method": "submitUpInfo",}
+json = {}
        
 response = requests.post(base_url, json=json)
 print(response.text)
