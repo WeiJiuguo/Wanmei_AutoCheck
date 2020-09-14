72cab113d96cbea33d3f380426b7c04b29833d95
Yooking <wangyukun55555@gmail.com>
2020-09-14 09:17:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ jsons =  {
             "deptid": deptId,
             "text": text
         },
-        "areaStr": "{\"streetNumber\":\"\",\"street\":\"长椿路辅路\",\"district\":\"中原区\",\"city\":\"郑州市\",\"province\":\"河南省\",\"town\":\"\",\"pois\":\"河南工业大学(莲花街校区)\",\"lng\":113.55064699999795,\"lat\":34.83870696238093,\"address\":\"中原区长椿路辅路河南工业大学(莲花街校区)\",\"text\":\"河南省-郑州市\",\"code\":\"\"}",
+        "areaStr": "{\"streetNumber\":\"\",\"street\":\"长椿路辅路\",\"district\":\"中原区\",\"city\":\"郑州市\",\"province\":\"河南省\",\"town\":\"\",\"pois\":\"河南工业大学(莲花街校区)\",\"lng\":113.55064699999795 + random.random()/1000,\"lat\":34.83870696238093 + random.random()/1000,\"address\":\"中原区长椿路辅路河南工业大学(莲花街校区)\",\"text\":\"河南省-郑州市\",\"code\":\"\"}",
         "reportdate": round(time.time()*1000),
         "customerid": "43",
         "deptid": deptId,
